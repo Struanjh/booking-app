92774acdd9efb1cbf93b995ed6d66a506728d816
STRUAN HENDERSON <Struanjh@gmail.com>
2023-07-13 04:51:19 +0100

    Modified forms to accept and display post req data

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -6,9 +6,16 @@ from app.forms import LoginForm
 def home_route():
     return render_template('index.html', title='Home')  
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login_view():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('This app received your username: {} and password {}'
+        .format(
+            form.username.data,
+            form.password.data
+        ))
+        return redirect('/login')
     return render_template('login_register.html', title='Login', form=form)   
 
 
