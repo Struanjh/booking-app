1068fdfcf690829155b749c697257331d68da768
STRUAN HENDERSON <Struanjh@gmail.com>
2023-07-15 04:54:21 +0100

    added role model and established relationship with users model: :

--- app/models.py
+++ app/models.py
@@ -2,7 +2,15 @@
 from app import db
 from datetime import datetime
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(64), nullable=False)
+    users = db.Relationship('User', backref='role')
+
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
@@ -10,19 +18,19 @@ class User(db.Model):
     pw_hash = db.Column(db.String(128))
     join_date = db.Column(db.DateTime, index=True)
     last_login = db.Column(db.DateTime, index=True)
-    role = db.Column(db.String(64))
     oauth = db.Column(db.Boolean)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
 
 
 class Classes(db.Model):
+    __tablename__ = 'classes'
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<Class starts {} and finishes {}>'.format(self.start_time, self.end_time)
-
 
