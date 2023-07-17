b5a66695144ad859f97e8cc31815ddbf771b8303
STRUAN HENDERSON <Struanjh@gmail.com>
2023-07-17 04:12:38 +0100

    Assoc class-bookings table

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 
+## ASSOC TABLE TO MANAGE MANY-MANY RELATIONSHIP BETWEEN CLASSES AND USERS
+class_bookings = db.Table(
+    'class_bookings',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('class_id', db.Integer, db.ForeignKey('english_classes.id'))
+)
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -23,10 +29,17 @@ class User(UserMixin, db.Model):
     last_login = db.Column(db.DateTime, index=True)
     oauth = db.Column(db.Boolean)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    ##ARGS -> 1-other side of many to many relationship. 2-assoc table, 3-reference used for this model from other side of many-many
+    classes = db.Relationship(
+        'EnglishClasses',
+        secondary=class_bookings,
+        backref=db.backref('students', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
-        
+
     def set_password(self, pw):
         self.pw_hash = generate_password_hash(pw)
 
@@ -40,12 +53,13 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-class Classes(db.Model):
-    __tablename__ = 'classes'
+class EnglishClasses(db.Model):
+    __tablename__ = 'english_classes'
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<Class starts {} and finishes {}>'.format(self.start_time, self.end_time)
+
 
