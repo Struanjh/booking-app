474400c94fd1b67614029f1e27bccf8b1bbc6765
STRUAN HENDERSON <Struanjh@gmail.com>
2023-07-25 04:12:23 +0100

    Added environment variables file and load env variables in config file

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,3 +11,18 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OAUTH2_PROVIDERS = {
+        # Google OAuth 2.0 documentation:
+        # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
+        'google': {
+            'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
+            'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
+            'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+            'token_url': 'https://accounts.google.com/o/oauth2/token',
+            'userinfo': {
+                'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+                'email': lambda json: json['email'],
+            },
+            'scopes': ['https://www.googleapis.com/auth/userinfo.email']
+        }
+    }
